91553e0042d178e2fa9b1a909ad849dbe7578836
d-meizin <d.meizin@gmail.com>
2023-09-15 16:41:59 +0300

    added class GameFrame

---
+++ com/chapter14/GameFrame.java
@@ -0,0 +1,52 @@
+package com.chapter14;
+import java.awt.FlowLayot;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JText.Field;
+import javax.swing.JTextField;
+
+class GameFrame extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	
+	int randomNumber = new Random().nextInt(10) + 1;
+	int numGuesses = 0;
+	
+	JTextField textField = new JTextField(15);
+	JButton button = new JButton("Сделать попытку");
+	JLabel label = new JLabel(numGuesses + " попыток");
+	
+	public GameFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		add(textField);
+		add(button);
+		add(label);
+		button.addActionListener(this);
+		pack();
+		setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String textFieldText = textField.getText();
+		
+		if (Integer.parseInt(textFieldText)==randomNumber) {
+			button.setEnable(false);
+			textField.setText(textField.getText() + "Молодец! Угадал!");
+			textField.setEnable(false);
+		} else {
+			textField.setText("");
+			textField.requestFocus();
+		}
+		
+		numGuesses++;
+		String guessWord = (numGuesses == 1) ? " попытка" : " попыток";
+		label.setText(numGuesses + guessWord);
+	}
+
+}
